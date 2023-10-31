1303b628398d8cd49b16cba83bbcb536ef773d59
Terry Li <terryseanli@gmail.com>
2023-10-31 00:01:18 -0700

    Adds placeholder views for clauses.

--- clauses/urls.py
+++ clauses/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /clauses/
     path("", views.index, name="index"),
+    # ex: /clauses/5/
+    path("<int:clause_id>/", views.detail, name="detail"),
+    # ex: /clauses/5/results/
+    path("<int:clause_id>/rating/", views.rating, name="rating"),
+    # ex: /clauses/5/vote/
+    path("<int:clause_id>/vote/", views.vote, name="vote"),
 ]
--- clauses/views.py
+++ clauses/views.py
@@ -3,3 +3,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the clauses index.")
+
+
+def detail(request, clause_id):
+    return HttpResponse("You're looking at clause %s." % clause_id)
+
+
+def rating(request, clause_id):
+    response = "You're looking at the rating of clause %s."
+    return HttpResponse(response % clause_id)
+
+
+def vote(request, clause_id):
+    return HttpResponse("You're voting on clause %s." % clause_id)
